67f8fd1b2c5d88b1a3f49b7147089883d8c8cf53
annun <annun@DESKTOP-TUF8GF9>
2022-08-16 12:53:12 +0530

    Add project files.

---
+++ Assignment_ClassesAndObjects/Q1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_ClassesAndObjects
+{
+    internal class Q1
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
