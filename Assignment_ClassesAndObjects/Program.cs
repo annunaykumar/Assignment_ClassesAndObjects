536ad8694a62ff397e8d2c148a151d3d5c623ba7
annunaykumar <52344382+annunaykumar@users.noreply.github.com>
2022-08-17 01:34:48 +0530

    Add files via upload

--- Assignment_ClassesAndObjects/Program.cs
+++ Assignment_ClassesAndObjects/Program.cs
@@ -10,7 +10,36 @@ namespace Assignment_ClassesAndObjects
     {
         static void Main(string[] args)
         {
-            
+          
+            {
+                char agn;
+                do
+                {
+                    fuctions k = new fuctions();
+                    string str=" ";
+                    Console.WriteLine("Please Select Any Function.");
+                    Console.WriteLine("\nPress D for Deposit an Amount. \nPress W for Withdraw an Amount.");
+                    str =(Console.ReadLine());
+                    switch (str)
+                    {
+                        case "D":
+                            k.fun1();
+                            break;
+                        case "W":
+                            k.fun2();
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Selection!!!");
+                            break;
+                    }
+                    Console.WriteLine("\nDo you want to continue this program? (y/n)");
+                    agn = Convert.ToChar(Console.ReadLine());
+
+                } while (agn == 'y');
+
+                Console.ReadKey();
+            }
         }
     }
 }
+     
