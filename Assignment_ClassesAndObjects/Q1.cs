caa8b51855b60a564be8277b54837a51513a4823
annunaykumar <52344382+annunaykumar@users.noreply.github.com>
2022-08-17 08:45:34 +0530

    Add files via upload

--- Assignment_ClassesAndObjects/Q1.cs
+++ Assignment_ClassesAndObjects/Q1.cs
@@ -10,7 +10,24 @@ namespace Assignment_ClassesAndObjects
     {
         public static void Main()
         {
+            double payment, debt;
+            int month;
+            Console.WriteLine("Please enter the monthly payment");
+            payment = Convert.ToDouble(Console.ReadLine());
+            month = 0;
+            debt = 1000;
+            Console.WriteLine("Month:" + month + "Balance:$" + debt + "Total payed:$" + "0.000");
 
+            while (debt > payment)
+            {
+                debt = debt * 1.015;
+                debt = debt - payment;
+                Console.WriteLine("Month: " + (month = month + 1) + "   Balance: $" + debt + "Total payed: $" + (payment * month));
+            }
+            Console.WriteLine("Month: " + (month = month + 1) + "   Balance: $0.0" + "Total payed: $" + (payment * month - debt));
+            Console.ReadLine();
         }
     }
 }
+    
+
